9e37617988cf927ee1b7388600b5cc729a209b60
Musa <Musasayed909@gmail.com>
2026-05-07 23:02:17 +0200

    added a couple things to main controller

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,4 +1,12 @@
 package app.controllers;
 
+import io.javalin.http.Context;
+
 public class MainController {
+
+    public static void index(Context ctx) {
+        ctx.render("index.html");
+    }
+
+
 }
