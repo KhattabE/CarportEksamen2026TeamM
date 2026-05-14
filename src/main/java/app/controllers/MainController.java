aa427310168a28d99897f49471ddc84998e3a426
Musa <Musasayed909@gmail.com>
2026-05-14 20:45:38 +0200

    Made some fixes in routes and added som methods to controllers and fixed a little html

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,18 +1,18 @@
 package app.controllers;
 
+import app.Main;
 import app.entities.Carport;
 import app.entities.CarportRequest;
+import app.entities.Material;
 import app.entities.Quote;
 import app.entities.User;
-import io.javalin.http.Context;
-import app.Main;
-import app.entities.Material;
 import app.persistence.MaterialMapper;
-import java.util.List;
+import io.javalin.http.Context;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MainController {
 
@@ -21,18 +21,22 @@ public class MainController {
     }
 
     public static void adminDashboard(Context ctx) {
+        ctx.attribute("currentPage", "dashboard");
         ctx.render("admin-dashboard.html");
     }
 
     public static void adminViewRequests(Context ctx) {
+        ctx.attribute("currentPage", "requests");
         ctx.render("admin-view-requests.html");
     }
 
     public static void adminRequestDetails(Context ctx) {
+        ctx.attribute("currentPage", "requests");
         ctx.render("admin-Request-Details.html");
     }
 
     public static void adminViewOrders(Context ctx) {
+        ctx.attribute("currentPage", "orders");
         ctx.render("admin_view_orders.html");
     }
 
@@ -41,11 +45,11 @@ public class MainController {
 
         List<Material> materials = materialMapper.getAllMaterials();
 
+        ctx.attribute("currentPage", "products");
         ctx.attribute("materials", materials);
 
         ctx.render("admin-products-and-price.html");
     }
-
 
     public static void profileQuoteDetails(Context ctx) {
 
@@ -56,11 +60,38 @@ public class MainController {
             return;
         }
 
-        Carport carport = new Carport(240, 230, 500, true, 230, 200, "Fladt tag");
+        Carport carport = new Carport(
+                240,
+                230,
+                500,
+                true,
+                230,
+                200,
+                "Fladt tag"
+        );
 
-        CarportRequest carportRequest = new CarportRequest(1, user.getUserId(), carport, "PENDING", "Kan man gøre plads til en motorcykel ved siden af?", LocalDateTime.now());
+        CarportRequest carportRequest = new CarportRequest(
+                1,
+                user.getUserId(),
+                carport,
+                "PENDING",
+                "Kan man gøre plads til en motorcykel ved siden af?",
+                LocalDateTime.now()
+        );
 
-        Quote quote = new Quote(1, 1, user.getUserId(), new BigDecimal("39395"), "AFVENTER BEHANDLING", "Sælger har justeret målene en smule.", LocalDate.now().plusDays(14), LocalDateTime.now(), null, null, null);
+        Quote quote = new Quote(
+                1,
+                1,
+                user.getUserId(),
+                new BigDecimal("39395"),
+                "AFVENTER BEHANDLING",
+                "Sælger har justeret målene en smule.",
+                LocalDate.now().plusDays(14),
+                LocalDateTime.now(),
+                null,
+                null,
+                null
+        );
 
         ctx.attribute("user", user);
         ctx.attribute("carportRequest", carportRequest);
