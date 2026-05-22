8b2e4dd28306c2bab1b0e1c47d6765f923f3a037
rasull9371 <rasullhossein250@gmail.com>
2026-05-22 03:18:49 +0200

    Added unit to QuoteMaterialLine and edited other code to be compatible with this and made it so admin-Request-Details.html displays the specific unit of the material rather than simply saying "stk" for all types of material

--- src/main/java/app/entities/QuoteMaterialLine.java
+++ src/main/java/app/entities/QuoteMaterialLine.java
@@ -9,15 +9,17 @@ public class QuoteMaterialLine {
     private int lengthCm;
     private BigDecimal quantity;
     private BigDecimal unitPrice;
+    private String unit;
     private String usageDescription;
 
-    public QuoteMaterialLine(int quoteLineId, int quoteId, int materialId, int lengthCm, BigDecimal quantity, BigDecimal unitPrice, String usageDescription) {
+    public QuoteMaterialLine(int quoteLineId, int quoteId, int materialId, int lengthCm, BigDecimal quantity, BigDecimal unitPrice, String unit, String usageDescription) {
         this.quoteLineId = quoteLineId;
         this.quoteId = quoteId;
         this.materialId = materialId;
         this.lengthCm = lengthCm;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
+        this.unit = unit;
         this.usageDescription = usageDescription;
     }
 
@@ -43,6 +45,10 @@ public class QuoteMaterialLine {
 
     public BigDecimal getUnitPrice() {
         return unitPrice;
+    }
+
+    public String getUnit() {
+        return unit;
     }
 
     public String getUsageDescription() {
--- src/main/java/app/services/CalculatedMaterialLine.java
+++ src/main/java/app/services/CalculatedMaterialLine.java
@@ -7,15 +7,17 @@ public class CalculatedMaterialLine {
 
     private int materialId;
     private String materialName;
+    private String unit;
     private int lengthCm;
     private int quantity;
     private BigDecimal unitPrice;
     private String usageDescription;
     private boolean pricePerMeter;
 
-    public CalculatedMaterialLine(int materialId, String materialName, int lengthCm, int quantity, BigDecimal unitPrice, String usageDescription, boolean pricePerMeter) {
+    public CalculatedMaterialLine(int materialId, String materialName, String unit, int lengthCm, int quantity, BigDecimal unitPrice, String usageDescription, boolean pricePerMeter) {
         this.materialId = materialId;
         this.materialName = materialName;
+        this.unit = unit;
         this.lengthCm = lengthCm;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
@@ -23,8 +25,12 @@ public class CalculatedMaterialLine {
         this.pricePerMeter = pricePerMeter;
     }
 
+    public CalculatedMaterialLine(int materialId, String materialName, int lengthCm, int quantity, BigDecimal unitPrice, String usageDescription, boolean pricePerMeter) {
+        this(materialId, materialName, "stk.", lengthCm, quantity, unitPrice, usageDescription, pricePerMeter);
+    }
+
     public CalculatedMaterialLine(int materialId, int lengthCm, int quantity, BigDecimal unitPrice, String usageDescription, boolean pricePerMeter) {
-        this(materialId, usageDescription, lengthCm, quantity, unitPrice, usageDescription, pricePerMeter);
+        this(materialId, usageDescription, "stk.", lengthCm, quantity, unitPrice, usageDescription, pricePerMeter);
     }
 
     public int getMaterialId() {
@@ -33,6 +39,10 @@ public class CalculatedMaterialLine {
 
     public String getMaterialName() {
         return materialName;
+    }
+
+    public String getUnit() {
+        return unit;
     }
 
     public int getLengthCm() {
